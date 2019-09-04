6044ecebc288f0e8f469ff034a4fa87debb11d7c
Jack Humbert <jack.humb@gmail.com>
2019-09-04 08:40:28 -0400

    correct stem size, pixels?

--- hint.sh
+++ hint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-/c/Users/Jack/Downloads/release-x64/otfccdump.exe font.otf -o font.otd
+/c/Users/Jack/Downloads/release-x64/otfccdump.exe fontforge-15.otf -o font.otd
 ideohint otd2hgl font.otd -o glyphlist.hgl --all
 ideohint hint glyphlist.hgl -o decisions.hgi --parameters param.toml
 ideohint instruct decisions.hgi -o instructs.hgs --parameters param.toml
 ideohint apply instructs.hgs font.otd -o out.otd --parameters param.toml
-/c/Users/Jack/Downloads/release-x64/otfccbuild.exe out.otd -o font.ttf
-rm glyphlist.hgl decisions.hgi instructs.hgs
+/c/Users/Jack/Downloads/release-x64/otfccbuild.exe out.otd -o fontforge-15.ttf
+rm font.otd glyphlist.hgl decisions.hgi instructs.hgs
